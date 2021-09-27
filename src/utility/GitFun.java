d0ea85af9eb4f2e5b2d300b206f6f4ffb943ddd4
xamsiye <xamsiye428@gmail.com>
2021-09-27 00:19:26 -0400

    add gitFun.java

---
+++ src/utility/GitFun.java
@@ -0,0 +1,14 @@
+package utility;
+
+public class GitFun {
+    public static void main(String[] args) {
+
+
+        System.out.println("every one will be fine" +
+                "");
+
+
+
+
+    }
+}
